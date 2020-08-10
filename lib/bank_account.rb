f7b774473150f782e14726d7b75b4b445edae004
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-10 22:13:06 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,39 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+      # can initialize a Bank Account (an instance of BankAccount Class)
+  def initialize(name)  # initializes with a name
+    @name = name        # can't change its name
+    @balance = 1000     # always initializes with balance of 1000
+    @status = "open"    # always initializes with a status of 'open'
+  end 
+  
+  def deposit(money)    # can deposit money into its account
+    @balance += money
+  end
+
+  def display_balance    # can display its balance 
+    "Your balance is $#{@balance}."
+  end 
+  
+  def valid?      #  is valid with an open status and a balance greater than 0
+    @status == "open" && @balance > 0 
+  end 
+  
+  def close_account     # can close its account 
+    @status = "closed"
+  end 
+  
+  def withdrawl(amount)
+    if @status == "open" && @balance >= amount
+        @balance -= amount
+        return true
+      end
+      false
+    end  
+  
+  
+  
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,49 @@
 class Transfer
-  # your code here
-end
+  
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount) #     can initialize a Transfer 
+    @sender = sender                       #     initializes with a sender
+    @receiver = receiver                   #     initializes with a receiver  
+    @amount = amount                       #     always initializes with a status of 'pending' 
+    @status = "pending"                    #     initializes with a transfer amount    
+ 
+  end
+  
+  def valid?   # can check that both accounts are valid 
+    @sender.valid? && @receiver.valid?
+  end 
+  
+  def execute_transaction    #can execute a successful transaction between two accounts
+    # binding.pry
+    if @status != "complete" 
+      # binding.pry
+      withdrawl_complete = @sender.withdrawl(@amount)
+      if withdrawl_complete && self.valid?   #    each transfer can only happen once
+        #  binding.pry
+        @receiver.deposit(@amount)
+        @status = "complete"
+      else
+        @status = "rejected"   #    rejects a transfer if the sender does not have enough funds (does not have a valid account)
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+  end
+
+  def reverse_transfer   #    can reverse a transfer between two accounts
+    # binding.pry
+    if @status == "complete"     #     it can only reverse executed transfers
+      @status = "pending"
+      temp_receiver = @sender
+      temp_sender =  @receiver
+      @sender = temp_sender
+      @receiver = temp_receiver
+      self.execute_transaction
+      if @status == "complete"
+        @status = "reversed"
+      end
+    end
+
+  end
+
+end 
